a8a29aa1b5e09408b6427b6b176c3d0b1432c719
omutlu221 <Omutlu221@gmail.com>
2026-04-18 17:56:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
+const OWNER_ID = "5981189137";
+
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "🔥 DelayHunter aktif!");
 });
 
 bot.on('message', (msg) => {
   if (msg.text !== "/start") {
-    bot.sendMessage(msg.chat.id, "👀 Mesajını aldım: " + msg.text);
+    bot.sendMessage(msg.chat.id, "👀 Mesajını aldım.");
+
+    bot.sendMessage(
+      OWNER_ID,
+      `📩 Yeni Mesaj\n👤 ${msg.from.first_name}\n🆔 ${msg.from.id}\n💬 ${msg.text}`
+    );
   }
 });
